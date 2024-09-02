954e4d0b25baab6aaa638d0336c1c9fb63a0e385
mahmutsamiseyhan <1911404042@ogr.mehmetakif.edu.tr>
2024-09-02 17:07:44 +0300

    Projenin ilk commit mesajı

--- helpers/send-mail.js
+++ helpers/send-mail.js
@@ -1,31 +1,29 @@
-// 'nodemailer' modülünü yükler. Bu modül, e-posta gönderimi için kullanılır.
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-// Yapılandırma bilgilerini içeren 'config' dosyasını yükler. Genellikle bu dosya e-posta kimlik bilgilerini ve diğer ayarları içerir.
-const config = require("../config");
-
-// E-posta taşıyıcısını oluşturur ve yapılandırır.
-// 'nodemailer.createTransport', e-posta göndermek için gerekli bağlantı ve yetkilendirme ayarlarını sağlar.
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Gmail kullanılıyor, başka bir sağlayıcı kullanıyorsanız değiştirin
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
-        user: process.env.EMAIL_USERNAME || config.email.username, // Çevre değişkeninden e-posta adresini alır veya yapılandırma dosyasından alır
-        pass: process.env.EMAIL_PASSWORD || config.email.password // Çevre değişkeninden şifreyi alır veya yapılandırma dosyasından alır
-    }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
-// E-posta sunucusuna bağlantıyı doğrular.
-// 'transporter.verify' yöntemi, yapılandırmanın doğru olup olmadığını kontrol eder.
-transporter.verify((error, success) => {
-    if (error) {
-        // Bağlantı hatası oluşursa, hatayı konsola yazdırır.
-        console.error("E-posta sunucusuna bağlantı hatası:", error);
-    } else {
-        // Bağlantı başarılı olursa, başarı mesajını konsola yazdırır.
-        console.log("E-posta sunucusuna bağlantı başarılı");
-    }
-});
+function sendMail(to, subject, text) {
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: to,
+        subject: subject,
+        text: text,
+    };
 
-// E-posta taşıyıcısını dışa aktarır.
-// Bu, diğer dosyalarda veya modüllerde kullanılmak üzere 'transporter' nesnesini sağlar.
-module.exports = transporter;
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error sending email:', error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports = sendMail;
