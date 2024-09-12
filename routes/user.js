a6ebd332a79d1e95e9a27fad75901f751c2c4c39
mahmutsamiseyhan <1911404042@ogr.mehmetakif.edu.tr>
2024-09-12 19:49:43 +0300

    Projenin ilk commit mesajı

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,52 @@ const router = express.Router();
 // User controller'ını dahil et
 const userController = require("../controllers/user");
 
+const Blog = require("../models/blog");
+const Category = require("../models/category");
+// Blogları listeleme, filtreleme ve sıralama
+router.get("/blogs", async (req, res) => {
+    const query = req.query.search || ''; // Arama sorgusu
+    const selectedCategory = req.query.category || ''; // Seçilen kategori
+    const sort = req.query.sort || 'createdAt'; // Sıralama kriteri
+    const order = req.query.order === 'asc' ? 1 : -1; // Sıralama yönü: asc veya desc
 
+    let filter = {}; // Blogları filtrelemek için kullanılacak obje
+
+    if (query) {
+        filter.baslik = { $regex: query, $options: 'i' }; // Başlıkta arama (case-insensitive)
+    }
+
+    if (selectedCategory) {
+        // Seçilen kategoriye göre blogları filtrele
+        const category = await Category.findOne({ _id: selectedCategory }).populate('blogs').exec();
+        filter._id = { $in: category.blogs.map(blog => blog._id) }; // Kategoriye ait blogların ID'lerini filtrele
+    }
+
+    try {
+        // Blogları çek ve kategorilerle populate et, aynı zamanda sıralama yap
+        const blogs = await Blog.find(filter)
+            .populate('categories')
+            .sort({ [sort]: order })
+            .exec();
+
+        // Mevcut kategorileri çek
+        const categories = await Category.find({}).exec();
+
+        // Sayfayı render et
+        res.render('users/blogs', { 
+            blogs, 
+            categories, 
+            query, 
+            selectedCategory,
+            sort,
+            order,
+            title: 'Blog Listesi'
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
 // Belirli bir kategoriye ait blog yazılarını listeleme endpoint'i
 // URL: /blogs/category/:slug
 // :slug, kategori adını temsil eder
