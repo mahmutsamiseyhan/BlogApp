8b96acea7cd75f9caa4892b4c7c64b79843f737f
mahmutsamiseyhan <1911404042@ogr.mehmetakif.edu.tr>
2024-09-12 20:24:49 +0300

    Projenin ilk commit mesajı

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,7 @@
 const express = require("express");
 // Yeni bir Express router nesnesi oluşturur
 const router = express.Router();
-// User controller'ını dahil et
-const userController = require("../controllers/user");
-
+// Modelleri dahil et
 const Blog = require("../models/blog");
 const Category = require("../models/category");
 
@@ -44,27 +42,33 @@ router.get("/blogs", async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
-// Belirli bir kategoriye ait blog yazılarını listeleme endpoint'i
-// URL: /blogs/category/:slug
-// :slug, kategori adını temsil eder
-// Örneğin: /blogs/category/technology gibi bir URL, 'technology' kategorisindeki blogları listeler
-router.get("/blogs/category/:slug", userController.blog_list);
 
 // Belirli bir blog yazısının detaylarını gösteren endpoint
 // URL: /blogs/:slug
 // :slug, blog yazısının başlığını veya tanımlayıcısını temsil eder
 // Örneğin: /blogs/my-first-post gibi bir URL, 'my-first-post' başlıklı blogun detaylarını gösterir
-router.get("/blogs/:slug", userController.blogs_details);
-
-// Tüm blog yazılarını listeleme endpoint'i
-// URL: /blogs
-// Bu endpoint, veritabanındaki tüm blogları listeler
-router.get("/blogs", userController.blog_list);
+router.get("/blogs/:slug", async (req, res) => {
+    try {
+        const blog = await Blog.findOne({ url: req.params.slug }).populate('categories').exec();
+        if (!blog) {
+            return res.status(404).send('Blog bulunamadı');
+        }
+        res.render('users/blog-detail', { 
+            blog, 
+            title: blog.baslik 
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
 
 // Ana sayfa endpoint'i
 // URL: /
 // Bu endpoint, uygulamanın ana sayfasını render eder
-router.get("/", userController.index);
+router.get("/", (req, res) => {
+    res.render('index', { title: 'Ana Sayfa' });
+});
 
 // Router'ı dışa aktar
 // Bu, tanımlanan rotaların uygulamanın diğer bölümlerinde kullanılmasına olanak tanır
